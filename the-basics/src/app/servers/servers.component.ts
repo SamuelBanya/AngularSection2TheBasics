8b4838cee065bd2877ad59edc706aa2a31d1b42e
Samuel Banya <sbanya@fastmail.com>
2023-01-27 08:29:42 -0600

    Adding in-line 'template' to servers component to create in-line HTML file

--- the-basics/src/app/servers/servers.component.ts
+++ the-basics/src/app/servers/servers.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
 
+// NOTE: We changed 'templateUrl' to 'template' and defined our own in-line HTML file within the new 'template' key value:
 @Component({
   selector: 'app-servers',
-  templateUrl: './servers.component.html',
+  template: `
+    <app-server></app-server>
+    <app-server></app-server>`,
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent {
