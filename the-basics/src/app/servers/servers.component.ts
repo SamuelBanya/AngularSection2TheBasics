771b0381685100984903c98b1a12d54a2b6b0979
Samuel Banya <sbanya@fastmail.com>
2023-01-31 08:36:42 -0600

    Adding data binding example

--- the-basics/src/app/servers/servers.component.ts
+++ the-basics/src/app/servers/servers.component.ts
@@ -1,18 +1,34 @@
 import { Component } from '@angular/core';
 
 @Component({
-  // NOTE: The 'selector' tag works like an CSS selector where you can just add the '<app-servers>' tag throughout the application:
-  // PREVIOUS VERSION: (using normal tag syntax):
-  selector: 'app-servers',
   // PREVIOUS VERSION (using attribute syntax):
   // selector: '[app-servers]',
   // PREVIOUS VERSION (using class syntax): 
   // selector: '.app-servers',
-  template: `
-    <app-server></app-server>
-    <app-server></app-server>`,
+  // NOTE: The 'selector' tag works like an CSS selector where you can just add the '<app-servers>' tag throughout the application:
+  // PREVIOUS VERSION: (using normal tag syntax):
+  selector: 'app-servers',
+  // NOTE: 
+  // This version uses an in-line template:
+  // template: `
+  //   <app-server></app-server>
+  //   <app-server></app-server>`,
+  templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent {
+  allowNewServer = false;
+
+  // NOTE:
+  // The 'constructor' is the function that is executed when the component is created in Angular:
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000)
+  }
+
+  ngOnInit() {
+
+  }
 
 }
